9344e685389b463e75df63f419b32c6c30021bf3
Andej Nesterenko <mentatus.n@gmail.com>
2021-09-07 16:07:01 +0300

    rework

--- app/src/main/java/com/example/vita/placeholderbehaviordemo/ViewBelowAppBarBehavior.kt
+++ app/src/main/java/com/example/vita/placeholderbehaviordemo/ViewBelowAppBarBehavior.kt
@@ -1,10 +1,11 @@
 package com.example.vita.placeholderbehaviordemo
 
 import android.content.Context
-import android.support.design.widget.AppBarLayout
-import android.support.design.widget.CoordinatorLayout
 import android.util.AttributeSet
 import android.view.View
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.appbar.AppBarLayout
+
 
 /**
  * Кастомный Behavior, позволяющий View занимать ровно то место, которое осталось между AppBarLayout и нижнем краем экрана
@@ -12,16 +13,25 @@ import android.view.View
 class ViewBelowAppBarBehavior @JvmOverloads constructor(
         context: Context? = null, attrs: AttributeSet? = null) : CoordinatorLayout.Behavior<View>(context, attrs) {
 
-    override fun layoutDependsOn(parent: CoordinatorLayout?, child: View?, dependency: View?) = dependency is AppBarLayout
+    override fun layoutDependsOn(
+        parent: CoordinatorLayout,
+        child: View,
+        dependency: View
+    )= dependency is AppBarLayout
 
-    override fun onDependentViewChanged(parent: CoordinatorLayout?, child: View?, dependency: View?): Boolean {
+    override fun onDependentViewChanged(
+        parent: CoordinatorLayout,
+        child: View,
+        dependency: View
+    ): Boolean {
         val appBar = dependency as AppBarLayout
         // т.к. y - отрицательное число
         val currentAppBarHeight = appBar.height + appBar.y
-        val parentHeight = parent?.height ?: 0
+        val parentHeight = parent.height ?: 0
         val placeHolderHeight = (parentHeight - currentAppBarHeight).toInt()
-        child?.layoutParams?.height = placeHolderHeight
-        child?.requestLayout()
+        child.layoutParams?.height = placeHolderHeight
+        child.requestLayout()
         return false
     }
+
 }
